7616e342063187097ccbc808506f835a63accafd
Julia Oliveira <julia.oliveira.silva@outlook.com>
2023-11-07 17:02:55 -0300

    Validações no form e contagem de aplausos

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -29,7 +29,12 @@ export function Post({ author, publishedAt, content }) {
 	}
 
 	function handleNewCommentChange() {
+		event.target.setCustomValidity('');
 		setNewCommentText(event.target.value);
+	}
+
+	function handleNewCommentInvalid() {
+		event.target.setCustomValidity('Esse campo é obrigatório.');
 	}
 
 	function deleteComment(commentToDelete) {
@@ -39,6 +44,8 @@ export function Post({ author, publishedAt, content }) {
 
 		setComments(commentsWithoutDeletedOne);
 	}
+
+	const isNewCommentEmpty = newCommentText.length === 0;
 
 	return (
 		<article className={styles.post}>
@@ -80,10 +87,14 @@ export function Post({ author, publishedAt, content }) {
 					placeholder='Deixe um comentário'
 					onChange={handleNewCommentChange}
 					value={newCommentText}
+					onInvalid={handleNewCommentInvalid}
+					required
 				/>
 
 				<footer>
-					<button type='submit'>Publicar</button>
+					<button type='submit' disabled={isNewCommentEmpty}>
+						Publicar
+					</button>
 				</footer>
 			</form>
 
